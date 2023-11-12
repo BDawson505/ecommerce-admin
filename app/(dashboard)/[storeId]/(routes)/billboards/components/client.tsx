b84357f2a78e8f9a5274e09eb22fdcbefc2ee572
Bradley Dawson Development <BDawson@Bradley.local>
2023-11-12 20:45:23 +0000

    api routes for billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Plus } from "lucide-react";
 import { useRouter, useParams } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -36,6 +37,12 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       <Separator />
 
       <DataTable columns={columns} data={data} searchKey='label' />
+
+      <Heading title='API' description='API calls for billboards' />
+
+      <Separator />
+
+      <ApiList entityName='billboards' entityIdName='billboardId' />
     </>
   );
 };
