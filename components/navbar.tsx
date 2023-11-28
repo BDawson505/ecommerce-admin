a1b8f630e675f0659d8c19d169ebf76bd5a2c873
Bradley Dawson Development <BDawson@Bradley.local>
2023-11-28 23:22:21 +0000

    added components to the dashboard for revenue, sales, and stock plus the dark mode toggle

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
 
 import { MainNav } from "@/components/main-nav";
+import { ModeToggle } from "@/components/theme-toggle";
 import StoreSwitcher from "@/components/store-switcher";
 
 const Navbar = async () => {
@@ -25,6 +26,7 @@ const Navbar = async () => {
         <MainNav className='mx-6' />
 
         <div className='ml-auto flex items-center space-x-4'>
+          <ModeToggle />
           <UserButton afterSignOutUrl='/' />
         </div>
       </div>
